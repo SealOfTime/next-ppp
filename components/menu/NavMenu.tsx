6b3e96c249bc940050bf463cdbdca159c164bd53
Stephan <stefanstark77@gmail.com>
2022-08-20 10:37:00 +0300

    added auth vk onClick

--- components/menu/NavMenu.tsx
+++ components/menu/NavMenu.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useSession, signOut } from "next-auth/react";
 
 import Image from 'next/image';
 
@@ -7,11 +8,25 @@ import HeaderBurger from './Burger';
 
 const NavMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+  const {data: session} = useSession();
+
+  const settingsWindows = {
+    width: 600,
+    height: 400,
+  }
 
   const toggleMenu = useCallback(() => {
     setIsOpen((s) => !s);
     document.body.classList.toggle('fix-position');
   }, []);
+
+  const onAuth = () => {
+
+    if(session) signOut()
+    else window.open("/login", '', `width=${settingsWindows.width}, height=${settingsWindows.height}, top=${window.screen.availHeight / 2 - (settingsWindows.height / 2)}, left=${window.screen.availWidth / 2 - (settingsWindows.width / 2)}`);
+    
+  }
 
   return (
     <header className="header">
@@ -20,7 +35,7 @@ const NavMenu = () => {
           <Image src="/mainLogo.png" alt="logo" width={105} height={60}/>
         </div>
         <nav className="header__menu menu">
-          <HeaderMenuBody classActive="active-menu" isOpen={isOpen} items={[{ name: 'Войти', link: '/login' }]} />
+          <HeaderMenuBody classActive="active-menu" isOpen={isOpen} items={[{ name: session? 'Выход ': 'Войти', callback: onAuth }]} />
         </nav>
         <HeaderBurger onClick={toggleMenu} />
       </div>
