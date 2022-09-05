d19b560bcc5616fbac513b83fd44b116b4f4af56
Stephan <StefanStak77@gmail.com>
2022-09-05 21:33:17 +0300

    fix auth in the safari

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,16 @@
-import { useSession, signIn } from "next-auth/react";
-import { useEffect } from "react";
+import { useSession, signIn } from 'next-auth/react';
+import { useEffect } from 'react';
 
-const login = () => {
-	const {data: session, status} = useSession();
-	useEffect(() => {
-		if(session) {
-			window.close();
-		} else {
-			signIn();
-		}	
-	}, [session])
-	return null;
-}
+const Login = () => {
+  const { data: session } = useSession();
+  useEffect(() => {
+    if (session) {
+      window.close();
+    } else {
+      signIn('vk');
+    }
+  }, [session]);
+  return null;
+};
 
-export default login
+export default Login;
