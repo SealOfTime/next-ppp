cb1160d80e034d1a2de27d2ce148b23314277b41
Matvei Vdovitsyn <sealofthetime@gmail.com>
2022-09-11 15:48:46 +0300

    Moved everything to /src, changed fonts

--- src/components/HomePage/HomePageFullScreenImage.tsx
+++ src/components/HomePage/HomePageFullScreenImage.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import background from '../../public/content/backgroung.jpg';
+import background from '../../assets/background.jpg';
 
 const FullScreenImage = ({ className }) => (
   <div className={`${className}`}>
