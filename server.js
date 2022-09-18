de7c4ba9353314a2db765b4fce6c5f276b6808fd
Matvei Vdovitsyn <sealofthetime@gmail.com>
2022-09-18 22:08:04 +0000

    Fixes

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ var https = require('https');
 var fs = require('fs');
 
 const next = require('next')
-const port = parseInt(process.env.PORT, 10) || config.getPort()
+const port = 443;
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, dir: __dirname })
+const hostname = 'ppp.itmo.online'
+const app = next({ dev, hostname: hostname, port: port, dir: __dirname })
 const handle = app.getRequestHandler()
 
 
 var options = {
-  key: fs.readFileSync('./certs/privkey.pem'),
-  cert: fs.readFileSync('./certs/fullchain.pen'),
+  key: fs.readFileSync('/etc/letsencrypt/live/ppp.itmo.online/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/ppp.itmo.online/fullchain.pem'),
 };
 
 app.prepare().then(() => {
